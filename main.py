6036a7326486e6b4e1860eed5e954fde2079c642
Angel Bello <114355567+Abyzou1995@users.noreply.github.com>
2024-03-20 01:04:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ app = FastAPI()
 
 ## Datasets for API functions
 
-df_function_PlayTimeGenre=pd.read_parquet("Dataset/Function_Dataset/F1.parquet")
-df_function_UserForGenre=pd.read_parquet("Dataset/Function_Dataset/F2.parquet")
-df_function_Recommends=pd.read_parquet("Dataset/Function_Dataset/F3-4-5.parquet")
-df_function_Sentiment=pd.read_parquet("Dataset/Function_Dataset/F5.parquet")
-df_function_Item_Item=pd.read_parquet("Dataset/Function_Dataset/MLF1.parquet")
-##Functions development in https://github.com/Abyzou1995/PI01_DATA10_MLops_HENRY/blob/main/FunctionAPI_MLops.ipynb
+df_function_PlayTimeGenre=pd.read_parquet("Dataset/Function_Dataset/F1.parquet") ## Function 1
+df_function_UserForGenre=pd.read_parquet("Dataset/Function_Dataset/F2.parquet")## Function 2
+df_function_Recommends=pd.read_parquet("Dataset/Function_Dataset/F3-4-5.parquet")## Function 3 and 4
+df_function_Sentiment=pd.read_parquet("Dataset/Function_Dataset/F5.parquet")## Function 5
+df_function_Item_Item=pd.read_parquet("Dataset/Function_Dataset/MLF1.parquet")## Function ML
+##Functions development in https://github.com/Abyzou1995/PI1_ML_PT07/blob/main/Funciones.ipynb
 
-## ML model development in https://github.com/Abyzou1995/PI01_DATA10_MLops_HENRY/blob/main/ModelML_MLops.ipynb
+## ML model development in https://github.com/Abyzou1995/PI1_ML_PT07/blob/main/ML.ipynb
 
 
 ##API functions
@@ -45,7 +45,10 @@ def PlayTimeGenre(genero:str):## API function 1
     
         return {f'Año de lanzamiento con más horas jugadas para Género {genero}': respuesta}
 
-
+"""TEST
+PlayTimeGenre("ACTION")## Testing Genre Action
+{'Año de lanzamiento con más horas jugadas para Género Action': 2012}
+"""
 @app.get('/UserForGenre/{genero}')
 def UserForGenre(genero : str):## API function 2
     
@@ -74,6 +77,30 @@ def UserForGenre(genero : str):## API function 2
                    
         return {f"Usuario con más horas jugadas para Género {genero1}" : usuario, "Horas jugadas":respuesta}
 
+"""TEST
+UserForGenre("Action")## Testing function2
+{'Usuario con más horas jugadas para Género Action': 'Sp3ctre',
+ 'Horas jugadas': [{'Año': 2017, 'Horas': 722},
+  {'Año': 2016, 'Horas': 493},
+  {'Año': 2015, 'Horas': 5125},
+  {'Año': 2014, 'Horas': 2178},
+  {'Año': 2013, 'Horas': 2008},
+  {'Año': 2012, 'Horas': 6305},
+  {'Año': 2011, 'Horas': 2582},
+  {'Año': 2010, 'Horas': 1301},
+  {'Año': 2009, 'Horas': 1805},
+  {'Año': 2008, 'Horas': 4},
+  {'Año': 2007, 'Horas': 1880},
+  {'Año': 2006, 'Horas': 15},
+  {'Año': 2005, 'Horas': 356},
+  {'Año': 2004, 'Horas': 2124},
+  {'Año': 2003, 'Horas': 128},
+  {'Año': 2002, 'Horas': 4},
+  {'Año': 2001, 'Horas': 0},
+  {'Año': 2000, 'Horas': 1177},
+  {'Año': 1999, 'Horas': 1}]}
+"""
+
 @app.get('/UsersRecommend/{anio}')
 def UsersRecommend( anio : int ):## API function 3
     
@@ -93,7 +120,14 @@ def UsersRecommend( anio : int ):## API function 3
                 respuesta.append(di)
         
         return {f"Los juegos mas recomendados para el año {anio} son ": respuesta }
-    
+
+"""TEST
+UsersRecommend( 2010 ) ## Testing function 3
+{'Los juegos mas recomendados para el año 2010 son ': [{'Puesto 1': 'Team Fortress 2'},
+  {'Puesto 2': 'Killing Floor'},
+  {'Puesto 3': 'Alien Swarm'}]}
+"""
+
 @app.get('/UsersNotRecommend/{anio}')
 def UsersNotRecommend( anio : int ):## API function 4
     
@@ -114,6 +148,12 @@ def UsersNotRecommend( anio : int ):## API function 4
         
         return {f"Los juegos menos recomendados para el año {anio} son ": respuesta }
     
+"""TEST
+UsersNotRecommend(2012)
+{'Los juegos menos recomendados para el año 2012 son ': [{'Puesto 1': 'Red Faction®: Armageddon™'},
+  {'Puesto 2': 'Team Fortress 2'},
+  {'Puesto 3': "The Kings' Crusade"}]}
+"""
 
 @app.get('/Sentiment_Analysis/{anio}')
 def Sentiment_Analysis( anio : int ):## API function 5
@@ -131,6 +171,13 @@ def Sentiment_Analysis( anio : int ):## API function 5
             respuesta={"Positivo":positivo,"Neutral":neutral,"Negativo":negativo}
         return {f"El analisis de sentimiento para el año de lanzamiento {anio} es ": respuesta }
 
+"""TEST
+
+Sentiment_Analysis( 2010 )## Testing function N° 5
+{'El analisis de sentimiento para el año de lanzamiento 2010 es ': {'Positivo': 947,
+  'Neutral': 1854,
+  'Negativo': 255}}
+"""
 
 @app.get('/Game_Recommendation/{id}')
 def Game_Recommendation( id : int ):## API function ML
@@ -150,8 +197,16 @@ def Game_Recommendation( id : int ):## API function ML
             sim_score = sim_score[1:6]## Getting top score 5 movies
             movies_index = [i[0] for i in sim_score] ## Finding names
             recommendations = list(df_function_Item_Item['app_name'].iloc[movies_index])## Making the list
-    return {'titulo':id,'lista recomendada': recommendations}
+    return {'ID de juego':id,'lista recomendada': recommendations}
         
-
+"""TEST
+Game_Recommendation(10)## Testing ML function
+{'Id de juego': 10,
+ 'lista recomendada': ['Counter-Strike: Condition Zero',
+  'Counter-Strike: Source',
+  'Counter-Strike: Global Offensive',
+  'Insurgency',
+  'Team Fortress Classic']}
+"""
 
        
